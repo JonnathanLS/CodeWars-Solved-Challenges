2ea4e166c4fda47831282befcf242dcb188acfba
Jonnathan Lacerda Santos <jonnathan.lacerda.santos@gmail.com>
2020-04-28 01:05:37 -0300

    solution with added recursion

--- challenges/tribonacci-sequence/solution.ts
+++ challenges/tribonacci-sequence/solution.ts
@@ -1,4 +1,15 @@
-export function tribonacci(s: number[], n: number): number[] {
-	for (let i = 0; s.length < n; i++) s.push(s[i] + s[i + 1] + s[i + 2]);
-	return s.slice(0,n);	
+export const tribonacci = {
+	iteration: (s: number[], n: number): number[] => {
+		for (let i = 0; s.length < n; i++) s.push(s[i] + s[i + 1] + s[i + 2]);
+		return s.slice(0,n);	
+	},
+	recursive: (sequence: number[], n: number): number[] => {
+		const l = sequence.length, s = sequence;
+		if(n > 3) s.push(s[l-1] + s[l-2] + s[l-3]);
+		return n > 3 ? tribonacci.recursive(s, n - 1) 
+			: n == 1 ? [ s[0] ] 
+			: n == 2 ? [ s[0], s[1] ] 
+			: n == 0 ? [] : s ;
+	}
 }
+
